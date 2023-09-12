26c2bd145dc15a6649e7f95b0aedbf3f46995757
Afjunior18 <>
2023-09-12 19:36:35 +0000

    Create function createBoard, cards on board. Start function shuffleCards

--- assets/script.js
+++ assets/script.js
@@ -6,105 +6,108 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             name: 'Card01',
             img: 'assets/images/bart.webp',
-        }
+        },
         {
             name: 'Card02',
-            img: 'assets/images/bart02.webp',
-        }
+            img: 'assets/images/bart01.webp',
+        },
         {
             name: 'Card03',
             img: 'assets/images/family.webp',
-        }
+        },
         {
             name: 'Card04',
             img: 'assets/images/homer.webp',
-        }
+        },
         {
             name: 'Card01',
             img: 'assets/images/lisa.webp',
-        }
+        },
         {
             name: 'Card05',
-            img: 'assets/images/bart.webp',
-        }
-        {
-            name: 'Card06',
-            img: 'assets/images/bart.webp',
-        }
+            img: 'assets/images/mother.webp',
+        },
         {
             name: 'Card01',
             img: 'assets/images/bart.webp',
-        }
+        },
         {
             name: 'Card02',
-            img: 'assets/images/bart02.webp',
-        }
+            img: 'assets/images/bart01.webp',
+        },
         {
             name: 'Card03',
             img: 'assets/images/family.webp',
-        }
+        },
         {
             name: 'Card04',
             img: 'assets/images/homer.webp',
-        }
+        },
         {
             name: 'Card01',
             img: 'assets/images/lisa.webp',
-        }
+        },
         {
             name: 'Card05',
-            img: 'assets/images/bart.webp',
-        }
-        {
-            name: 'Card06',
-            img: 'assets/images/bart.webp',
-        }
+            img: 'assets/images/mother.webp',
+        },
     ];
+
+    // Getting references to HTML elements for later manipulation.
+
+    const board = document.getElementById('game-board');
+    const newMatch = document.getElementById('new-match');
+    const wrongMatch = document.getElementById('wrong-match');
+
+    // buttons, start and restart
+
+    const startGame = document.getElementById('start-game');
+    const restartGame = document.getElementById('restart-game');
+
+
+    // This function shuffles an array of cards.
+    // It generates a random order of the cards for a game.
+
+    function shuffleCards(cards) {
+        return cards.sort(() => Math.random() - 0.5);
+    }
+
+    // this function create the game board.
+
+    function createBoard() {
+        board.innerHTML = '';
+        cards.forEach(card => {
+            const cardElement = document.createElement('div');
+            const imgElement = document.createElement('img');
+            imgElement.src = card.img;
+            cardElement.appendChild(imgElement);
+            board.appendChild(cardElement);
+        });
+    }
+    createBoard();
+
+
+    //All function for use on Memory game
+
+    // function startGame() {
+
+    // }
+
+    function flipCards() {
+
+    }
+
+    function checkMatchingCards() {
+
+    }
+
+    function updateScore() {
+
+    }
+
+    function checkGameOver() {
+
+    }
+
+
 });
-
-// Getting references to HTML elements for later manipulation.
-
-const board = document.getElementById('game-board');
-const newMatch = document.getElementById('new-match');
-const wrongMatch = document.getElementById('wrong-match');
-
-// button, start and restart
-
-const startGame = document.getElementById('start-game');
-const restartGame = document.getElementById('restart-game');
-
-
-
-
-//All function for use on Memory game
-
-function startGame() {
-
-}
-
-function createBoard() {
-
-
-}
-
-function shuffleCards() {
-
-}
-
-
-
-function flipCards() {
-
-}
-
-function checkMatchingCards() {
-
-}
-
-function updateScore() {
-
-}
-
-function checkGameOver{
-
-}

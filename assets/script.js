7ab4a6438af2995a2effed9c06f786916b24ba00
Afjunior18 <>
2023-09-12 13:28:19 +0000

    Create names for all possible functions to be used in game development

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+
+//All function for use on Memory game
+
+function startGame() {
+
+}
+
+function createBoard() {
+
+}
+
+function shuffleCards() {
+
+}
+
+function flipCards() {
+
+}
+
+function checkMatchingCards() {
+
+}
+
+function updateScore() {
+
+}
+
+function checkGameOver{
+
+}

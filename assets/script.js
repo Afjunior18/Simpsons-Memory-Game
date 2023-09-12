0615b7d02616b4228af14a42d601e10881d2764a
Afjunior18 <>
2023-09-12 14:36:55 +0000

    Create eventListener DOMContentLoaded and declare an array with all objectfor the board game

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,68 @@
+// Wait for the page to finish loading before running any JavaScript.
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = [
+        {
+            name: 'Card01',
+            img: 'assets/images/bart.webp',
+        }
+        {
+            name: 'Card02',
+            img: 'assets/images/bart02.webp',
+        }
+        {
+            name: 'Card03',
+            img: 'assets/images/family.webp',
+        }
+        {
+            name: 'Card04',
+            img: 'assets/images/homer.webp',
+        }
+        {
+            name: 'Card01',
+            img: 'assets/images/lisa.webp',
+        }
+        {
+            name: 'Card05',
+            img: 'assets/images/bart.webp',
+        }
+        {
+            name: 'Card06',
+            img: 'assets/images/bart.webp',
+        }
+        {
+            name: 'Card01',
+            img: 'assets/images/bart.webp',
+        }
+        {
+            name: 'Card02',
+            img: 'assets/images/bart02.webp',
+        }
+        {
+            name: 'Card03',
+            img: 'assets/images/family.webp',
+        }
+        {
+            name: 'Card04',
+            img: 'assets/images/homer.webp',
+        }
+        {
+            name: 'Card01',
+            img: 'assets/images/lisa.webp',
+        }
+        {
+            name: 'Card05',
+            img: 'assets/images/bart.webp',
+        }
+        {
+            name: 'Card06',
+            img: 'assets/images/bart.webp',
+        }
+    ];
+});
+
+
 
 //All function for use on Memory game
 

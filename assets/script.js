1ac883f2803644a6fa628377a2dafacdeb4ce665
Afjunior18 <>
2023-09-13 11:17:03 +0000

    Add if statement to check if the card is flipped

--- assets/script.js
+++ assets/script.js
@@ -6,50 +6,62 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             name: 'Card01',
             img: 'assets/images/bart.webp',
+            flipped: false
         },
         {
             name: 'Card02',
             img: 'assets/images/bart01.webp',
+            flipped: false
         },
         {
             name: 'Card03',
             img: 'assets/images/family.webp',
+            flipped: false
         },
         {
             name: 'Card04',
             img: 'assets/images/homer.webp',
+            flipped: false
         },
         {
             name: 'Card01',
             img: 'assets/images/lisa.webp',
+            flipped: false
         },
         {
             name: 'Card05',
             img: 'assets/images/mother.webp',
+            flipped: false
         },
         {
             name: 'Card01',
             img: 'assets/images/bart.webp',
+            flipped: false
         },
         {
             name: 'Card02',
             img: 'assets/images/bart01.webp',
+            flipped: false
         },
         {
             name: 'Card03',
             img: 'assets/images/family.webp',
+            flipped: false
         },
         {
             name: 'Card04',
             img: 'assets/images/homer.webp',
+            flipped: false
         },
         {
             name: 'Card01',
             img: 'assets/images/lisa.webp',
+            flipped: false
         },
         {
             name: 'Card05',
             img: 'assets/images/mother.webp',
+            flipped: false
         },
     ];
 
@@ -66,36 +78,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
     runGame.addEventListener('click', startGame);
 
-    // quando clicar ira iniciar o jogo, as cartao serao embaralhadas
+
+    // Function to initiate the game.
+    // Shuffles the cards and creates the game board.
 
     function startGame() {
         const shuffledCards = shuffleCards(cards);
         createBoard();
     }
-
+    // calling the function to initiate the game
     startGame();
 
 
-    // This function shuffles an array of cards.
+    // This function shuffles the cards array.
     // It generates a random order of the cards for a game.
 
     function shuffleCards(cards) {
         return cards.sort(() => Math.random() - 0.5);
     }
 
+    // let cardsChosen = [];
+    // let cardsChosenId = [];
+    // let cardsWon = [];
+
+
     // this function create the game board.
 
     function createBoard() {
+        // Clear the existing content of the board element
         board.innerHTML = '';
+        // Loop through each card in the array
         cards.forEach(card => {
             const cardElement = document.createElement('div');
             const imgElement = document.createElement('img');
-            imgElement.src = card.img;
+
+            // Add a class to the image element for styling CSS
+            imgElement.classList.add('card-image');
+
+            // Check if the card is flipped (face up) or not (face down)
+            if (card.flipped) {
+                imgElement.src = card.img;
+            } else {
+                imgElement.src = 'assets/images/cover.webp';
+            }
+            // Append the image element to the card element
+            // Append the card element to the game board
             cardElement.appendChild(imgElement);
             board.appendChild(cardElement);
         });
     }
-    createBoard();
+
 
 
     // All function for use on Memory game
